de50bf4b8187ed7378a3ec9cd53b0a4afc182ec0
ognovais <159445979+ognovais@users.noreply.github.com>
2024-10-25 10:18:55 -0300

    Create questao1.py

---
+++ questao1.py
@@ -0,0 +1,10 @@
+a = '5'
+b = 2
+c = a * b
+print(c)
+
+# Relatório:
+# Saída obtida: 55
+# Resposta correta: 55
+# Resposta marcada: 10
+# Análise dos erros: O erro ocorreu porque a variável 'a' é uma string. A multiplicação de uma string por um número resulta na repetição da string, não na multiplicação numérica. Para obter 10, 'a' deveria ser um número (inteiro ou float).
