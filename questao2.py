68d831269f16af35a84235641e1d70bc9f2d1b95
ognovais <159445979+ognovais@users.noreply.github.com>
2024-10-25 10:19:12 -0300

    Create questao2.py

---
+++ questao2.py
@@ -0,0 +1,9 @@
+for i in range(1, 5):
+if i % 2 == 0:
+print(i, end=' ')
+
+# Relatório:
+# Saída obtida: 2 4 
+# Resposta correta: 2 4 
+# Resposta marcada: 2 4 
+# Análise dos erros: Não houve erro. O código está correto e imprime os números pares entre 1 e 5.
